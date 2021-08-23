aa13cfe6c82c29fc00fe8df1db48aba7d41836ae
anyung533 <860300966@qq.com>
2021-08-23 20:27:56 +0800

    ****************

---
+++ JOJO/file.sh
@@ -0,0 +1,14 @@
+#NINJA替换
+cp -rf /ql/repo/anyung533_jojo/file/favicon.ico /ql/ninja/backend/static/
+cp -rf /ql/repo/anyung533_jojo/file/index.html /ql/ninja/backend/static/
+#jdCookie.js
+cp -rf /ql/repo/anyung533_jojo/file/jdCookie.js /ql/scripts/
+
+
+
+
+
+
+
+
+	
--- JOJO/onlyone.sh
+++ JOJO/onlyone.sh
@@ -8,17 +8,8 @@ cd /ql/scripts && npm install crypto -g
 cd /ql/scripts && npm install download
 cd /ql/scripts && npm install -g typescript
 cd /ql/scripts && npm i -S png-js
-
-
-
 cd /ql/scripts && apk add libc6-compat
 cd /ql/scripts && apk add nodejs-current
-
-
 cd /ql/scripts && pip3 install requests
-
-
 cd /ql/scripts && apk add python3 zlib-dev gcc jpeg-dev python3-dev musl-dev freetype-dev
-
-
 cd /ql/scripts/ && apk add --no-cache build-base g++ cairo-dev pango-dev giflib-dev && npm i && npm i -S ts-node typescript @types/node date-fns axios png-js canvas --build-from-source
