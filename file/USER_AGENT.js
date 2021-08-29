b6d201c69a2cce136433e11bfb651ef4feefb394
anyung533 <860300966@qq.com>
2021-08-29 19:43:55 +0800

    656666666666

--- file/USER_AGENT.js
+++ file/USER_AGENT.js
@@ -48,7 +48,7 @@ function randomNumber(min = 0, max = 100) {
 const RANDOM_UA = USER_AGENTS[randomNumber(0, USER_AGENTS.length)];
 
 const GET_RANDOM_TIME_UA = () => {
-  return 'Mozilla/5.0 (iPhone; U; CPU iPhone OS 4_3_2 like Mac OS X; en-us) AppleWebKit/533.17.9 (KHTML, like Gecko) Version/5.0.2 Mobile/8H7 Safari/6533.18.5 UCBrowser/13.4.2.1122';
+  return 'Mozilla/5.0 (iPhone; CPU iPhone OS 15_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.0 Mobile/15E148 Safari/6533.18.5 QQBrowser/11.7.7.4862';
 };
 
 module.exports = {
