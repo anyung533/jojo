54c577fb693ab55e89b759ba582cb5fb4d88b756
anyung533 <860300966@qq.com>
2021-08-30 23:05:05 +0800

    夫人吩咐

--- JOJO/file.sh
+++ JOJO/file.sh
@@ -8,18 +8,12 @@ cp -rf /ql/repo/anyung533_jojo/file/code.sh /ql/config/
 cp -rf /ql/repo/anyung533_jojo/file/team.sh /ql/config/
 cp -rf /ql/repo/anyung533_jojo/file/task_before.sh /ql/config/
 
+cd /ql/scripts && npm install ws
+
+
  
 
 #更新NINJA
-rm -rf /ql/ninja/backend/ql.js
-rm -rf /ql/ninja/backend/user.js
-
-cd /ql/ninja/backend && wget https://raw.githubusercontent.com/MoonBegonia/ninja/deda6f0c0207f2bc0e10454fdbe99b3a9f3878a8/backend/ql.js
-
-cd /ql/ninja/backend && wget https://raw.githubusercontent.com/MoonBegonia/ninja/deda6f0c0207f2bc0e10454fdbe99b3a9f3878a8/backend/user.js
-cp -rf /ql/repo/anyung533_jojo/file/USER_AGENT.js /ql/ninja/backend/utils/
-pnpm install
-pm2 start
 
 
 
