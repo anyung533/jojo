bc3fc140fd217fc429f7b38c2e82e66be3e0cc4b
anyung533 <860300966@qq.com>
2021-09-05 15:20:47 +0800

    删除ninja

--- JOJO/file.sh
+++ JOJO/file.sh
@@ -1,15 +1,9 @@
-#NINJA替换
+#CK
+cp -rf /ql/repo/anyung533_jojo/ql_refreshCK.py /ql/scripts/
 
 #jdCookie.js
 cp -rf /ql/repo/anyung533_jojo/file/jdCookie.js /ql/scripts/
-#code.sh+team.sh
-cp -rf /ql/repo/anyung533_jojo/file/code.sh /ql/config/
-cp -rf /ql/repo/anyung533_jojo/file/team.sh /ql/config/
-cp -rf /ql/repo/anyung533_jojo/file/task_before.sh /ql/config/
 
-
-
- 
 
 #更新NINJA
 
