0ca79f19e9213fd879342391bb0ea202d151b713
anyung533 <860300966@qq.com>
2021-09-05 12:53:10 +0800

    no message

--- JOJO/file.sh
+++ JOJO/file.sh
@@ -1,6 +1,5 @@
 #NINJA替换
-cp -rf /ql/repo/anyung533_jojo/file/favicon.ico /ql/ninja/backend/static/
-cp -rf /ql/repo/anyung533_jojo/file/index.html /ql/ninja/backend/static/
+
 #jdCookie.js
 cp -rf /ql/repo/anyung533_jojo/file/jdCookie.js /ql/scripts/
 #code.sh+team.sh
@@ -8,7 +7,6 @@ cp -rf /ql/repo/anyung533_jojo/file/code.sh /ql/config/
 cp -rf /ql/repo/anyung533_jojo/file/team.sh /ql/config/
 cp -rf /ql/repo/anyung533_jojo/file/task_before.sh /ql/config/
 
-cd /ql/scripts && npm install ws
 
 
  
