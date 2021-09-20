4a60b83949b6c8097c63130a5212fc16b1217d05
anyung533 <860300966@qq.com>
2021-09-20 11:20:07 +0800

    no message

---
+++ file/dev.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+
+//青龙2.9机器人
+_"github.com/cdle/sillyGirl/develop/qinglong" 
+
+//京东账号
+_ "github.com/cdle/sillyGirl/develop/jd_cookie"
+
+//腾讯QQ
+_"github.com/cdle/sillyGirl/im/qq"
+
+//Te Legram
+_"github.com/cdle/sillyGirl/im/tg"
+)
+
