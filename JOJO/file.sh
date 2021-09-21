be605f82c6736ad9165100870348ba94be1c93ff
anyung533 <860300966@qq.com>
2021-09-21 22:11:59 +0800

    no message

--- JOJO/file.sh
+++ JOJO/file.sh
@@ -9,6 +9,7 @@ cp -rf /ql/repo/anyung533_jojo/utils/JDJRValidator_Pure.js /ql/scripts/
 cp -rf /ql/repo/anyung533_jojo/file/jdCookie.js /ql/scripts/
 cp -rf /ql/repo/anyung533_jojo/file/sign_graphics_validate.js /ql/scripts/
 cp -rf /ql/repo/anyung533_jojo/file/ql.js /ql/scripts/
+cp -rf /ql/repo/anyung533_jojo/file/code.sh /ql/config/
 #助力依赖
 cp -rf /ql/repo/anyung533_jojo/function /ql/scripts/
 
