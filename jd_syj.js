c8b2bad76c9f3d08850e1dd1aa542b83a5da8456
cestjojo <anyung533@users.noreply.github.com>
2021-11-24 16:22:19 +0800

    jd_CheckCK.js

--- jd_syj.js
+++ jd_syj.js
@@ -17,7 +17,7 @@ const $ = new Env('赚京豆');
 const notify = $.isNode() ? require('./sendNotify') : '';
 //Node.js用户请在jdCookie.js处填写京东ck;
 const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
-let jdNotify = true;//是否关闭通知，false打开通知推送，true关闭通知推送
+let jdNotify = false;//是否关闭通知，false打开通知推送，true关闭通知推送
 const randomCount = $.isNode() ? 0 : 5;
 //IOS等用户直接用NobyDa的jd cookie
 let cookiesArr = [], cookie = '', message;
