47a5903d4efad3d3fbe56141c3da42fb2fbcb6cc
anyung533 <860300966@qq.com>
2021-11-27 00:44:01 +0800

    no message

---
+++ pull.sh
@@ -0,0 +1,19 @@
+ql repo https://github.com/LJMX996/jd.git "jd_|jx_|gua_|code_|zy_|jd-task-|jddj_|ql_|ck_" "utils|static|tools|activity|backUp|jd_delCoupon|format_|tmp|cdle|helpjs|tmpcron" "" "aaron" 
+
+
+
+cp -rfv /ql/repo/anyung533_jojo/* /ql/scripts/
+
+cp -rfv /ql/repo/anyung533_jojo/* /ql/scripts/anyung533_jojo/
+
+
+
+
+
+
+cp -rfv /ql/repo/anyung533_jojo/pull.sh /ql/config/
+
+
+
+
+
