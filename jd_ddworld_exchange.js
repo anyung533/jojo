c653f74645267e26dd0c14ee374e023edc2b9fa6
cestjojo <anyung533@users.noreply.github.com>
2021-12-03 16:30:23 +0800

    jd_ddworld_exchange.js

--- jd_ddworld_exchange.js
+++ jd_ddworld_exchange.js
@@ -1,9 +1,9 @@
 
 /**
- 东东世界兑换
- cron 0 0 * * * jd_ddworld_exchange.js
- TG频道：https://t.me/sheeplost
- */
+东东世界兑换
+cron 0 0,16 * * * jd_ddworld_exchange.js
+TG频道：https://t.me/sheeplost
+*/
 const $ = new Env("东东世界兑换");
 const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
 const notify = $.isNode() ? require('./sendNotify') : '';
@@ -47,6 +47,9 @@ if ($.isNode()) {
                 continue
             }
             await main();
+            if (i != cookiesArr.length) {
+                await $.wait(5000)
+            }
         }
     }
 })().catch((e) => { $.log('', `❌ ${$.name}, 失败! 原因: ${e}!`, '') }).finally(() => { $.done(); })
@@ -61,9 +64,11 @@ async function main() {
             await task('get_exchange');
             if (!$.hotFlag) {
                 if ($.exchangeList) {
+                    $.exchangeList = $.exchangeList.filter((x) => !x.name.includes('红包'))
                     for (const vo of $.exchangeList.reverse()) {
                         $.log(`去兑换：${vo.name}`)
                         await taskPost('do_exchange', `id=${vo.id}`);
+                        await $.wait(1000)
                     }
                 } else {
                     $.log("没有获取到兑换列表！")
