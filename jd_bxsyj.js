f1717f35d4268f1621a42fd7b539a92f32e087cc
cestjojo <anyung533@users.noreply.github.com>
2021-12-11 09:22:35 +0800

    jd_bxsyj.js

--- jd_bxsyj.js
+++ jd_bxsyj.js
@@ -3,7 +3,7 @@
 脚本兼容: QuantumultX, Surge, Loon, JSBox, Node.js
 [task_local]
 #冰雪闪耀季
-30 0,2 7-24 12 * jd_bxsyj.js, tag=冰雪闪耀季, img-url=https://raw.githubusercontent.com/Orz-3/mini/master/Color/jd.png, enabled=true
+30 0,11 7-24 12 * jd_bxsyj.js, tag=冰雪闪耀季, img-url=https://raw.githubusercontent.com/Orz-3/mini/master/Color/jd.png, enabled=true
 
  */
 const $ = new Env('冰雪闪耀季');
