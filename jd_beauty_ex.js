e1660699a4806638025f3be1df9ad136b981faa6
cestjojo <anyung533@users.noreply.github.com>
2021-12-22 12:48:00 +0800

    jd_beauty_ex.js

--- jd_beauty_ex.js
+++ jd_beauty_ex.js
@@ -11,7 +11,7 @@ const notify = $.isNode() ? require('./sendNotify') : '';
 //Node.js用户请在jdCookie.js处填写京东ck;
 const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
 const WebSocket = require('ws');
-let jdNotify = false;//是否关闭通知，false打开通知推送，true关闭通知推送
+let jdNotify = true;//是否关闭通知，false打开通知推送，true关闭通知推送
 $.accountCheck = true;
 $.init = false;
 $.bean = '1'; //兑换多少豆，默认是500
