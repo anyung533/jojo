04e0bc0d6bcd150d8d1e1ff227b5767bbadcb166
cestjojo <anyung533@users.noreply.github.com>
2021-12-25 22:51:39 +0800

    file.sh

--- JOJO/file.sh
+++ JOJO/file.sh
@@ -1,20 +1,9 @@
 #CK
-cp -rf /ql/repo/anyung533_jojo/ql_refreshCK.py /ql/scripts/
-cp -rf /ql/repo/anyung533_jojo/utils/* /ql/scripts/
 
 
+cp -rfv /ql/repo/anyung533_jojo/* /ql/scripts/
 
-cp -rf /ql/repo/anyung533_jojo/utils/JDJRValidator_Pure.js /ql/scripts/
-#jdCookie.js
-cp -rf /ql/repo/anyung533_jojo/file/jdCookie.js /ql/scripts/anyung533_jojo/
-cp -rf /ql/repo/anyung533_jojo/file/sign_graphics_validate.js /ql/scripts/anyung533_jojo/
-cp -rf /ql/repo/anyung533_jojo/file/ql.js /ql/scripts/anyung533_jojo/
-cp -rf /ql/repo/anyung533_jojo/file/code.sh /ql/config/
-cp -rf /ql/repo/anyung533_jojo/file/task_before.sh /ql/config/
-#助力依赖
-cp -rf /ql/repo/anyung533_jojo/function /ql/scripts/anyung533_jojo/
-
-
+cp -rfv /ql/repo/anyung533_jojo/* /ql/scripts/anyung533_jojo/
 
 
 
