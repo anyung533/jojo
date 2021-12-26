3c0fcca12988852c6f77117686fde6196ae49a8f
cestjojo <anyung533@users.noreply.github.com>
2021-12-26 21:21:47 +0800

    jd_speed_sign_Part1.js

--- jd_speed_sign_Part2.js
+++ jd_speed_sign_Part2.js
@@ -1,5 +1,5 @@
 /*
-cron "0 7 * * *" jd_speed_sign_Part2.js, tag:京东极速版任务1
+cron "0 7,19 * * *" jd_speed_sign_Part2.js, tag:京东极速版任务1
 */
  //详细说明参考 https://github.com/ccwav/QLScript2.
 const $ = new Env('京东极速版任务2');
