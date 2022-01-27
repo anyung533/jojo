101bff71622e6e35c74f0a61a8a410f70b09868a
cestjojo <anyung533@users.noreply.github.com>
2022-01-28 00:55:38 +0800

    file.sh

--- JOJO/file.sh
+++ JOJO/file.sh
@@ -5,7 +5,7 @@ cp -rfv /ql/repo/anyung533_jojo/* /ql/scripts/
 
 cp -rfv /ql/repo/anyung533_jojo/* /ql/scripts/anyung533_jojo/
 
-
+cp -rfv /ql/repo/anyung533_jojo/scripts/code.sh /ql/scripts/anyung533_jojo/config
 
 
 
