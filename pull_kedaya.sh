94616e7293b39ee26f40acebebce81895e137a11
860300966@qq.com <860300966@qq.com>
2022-05-07 22:31:07 +0800

    no message

---
+++ pull_kedaya.sh
@@ -0,0 +1,25 @@
+rm -rf /ql/repo/qitoqito_kedaya
+
+ql repo https://github.com/qitoqito/kedaya.git kedaya
+
+cp -a /ql/repo/qitoqito_kedaya/. /ql/scripts
+
+
+
+task qitoCreat.js now
+
+cp -rfv /ql/scripts/jw_help_distributeBean.js /ql/scripts/jw_help_distributeBean_1.js
+
+cp -rfv /ql/scripts/jw_help_distributeBean.js /ql/scripts/jw_help_distributeBean_2.js
+
+cp -rfv /ql/scripts/jw_help_distributeBean.js /ql/scripts/jw_help_distributeBean_3.js
+
+cp -rfv /ql/scripts/jw_help_distributeBean.js /ql/scripts/jw_help_distributeBean_4.js
+
+cp -rfv /ql/scripts/parse/jd/jw_help_distributeBean.js /ql/scripts/parse/jd/jw_help_distributeBean_1.js
+
+cp -rfv /ql/scripts/parse/jd/jw_help_distributeBean.js /ql/scripts/parse/jd/jw_help_distributeBean_2.js
+
+cp -rfv /ql/scripts/parse/jd/jw_help_distributeBean.js /ql/scripts/parse/jd/jw_help_distributeBean_3.js
+
+cp -rfv /ql/scripts/parse/jd/jw_help_distributeBean.js /ql/scripts/parse/jd/jw_help_distributeBean_4.js
