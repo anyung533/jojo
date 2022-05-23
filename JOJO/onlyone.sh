c743a76f95c4c4bd0493eb3076920f5753526d22
cestjojo <46280959+anyung533@users.noreply.github.com>
2022-05-23 11:27:00 +0800

    /

--- JOJO/onlyone.sh
+++ JOJO/onlyone.sh
@@ -1,34 +1,37 @@
-
-npm install -g pnpm
-pnpm install -g @otplib/preset-default
-cd /ql/scripts && pnpm install -g js-base64
-cd /ql/scripts && pnpm install -g fund
-cd /ql/scripts && pnpm install -g jsdom
-cd /ql/scripts && pnpm install -g form-data
-cd /ql/scripts && pnpm install -g tough-cookie
-cd /ql/scripts && pnpm install -g axios date-fns
-cd /ql/scripts && pnpm install -g crypto-js
-cd /ql/scripts && pnpm install -g crypto -g
-cd /ql/scripts && pnpm install -g download
-cd /ql/scripts && pnpm install -g typescript
-cd /ql/scripts && pnpm install -g png-js
-
-cd /ql/scripts && pnpm install -g got@11
-cd /ql/scripts && pnpm install -g ts-node
-cd /ql/scripts && pnpm install -g @types/node
-cd /ql/scripts && pnpm install -g canvas
-
 cd /ql/scripts && pip3 install requests
 cd /ql/scripts && pip3 install --upgrade pip
-cd /ql/scripts && pip3 install aiohttp
 cd /ql/scripts && pip3 install jieba
-
+cd /ql/scripts && pip3 install telethon
+cd /ql/scripts && pip3 install qrcode
+cd /ql/scripts && pip3 install PyExecJS
+cd /ql/scripts && pip3 install aiohttp
+cd /ql/scripts && pip3 install PySocks
 
 cd /ql/scripts && apk add libc6-compat
 cd /ql/scripts && apk add nodejs-current
-
-
-cd /ql/scripts && apk add python3 zlib-dev gcc jpeg-dev python3-dev musl-dev freetype-dev
-
-    
+cd /ql/scripts && apk add python3 zlib-dev gcc jpeg-dev python3-dev musl-dev freetype-dev    
 cd /ql/scripts/ && apk add --no-cache build-base g++ cairo-dev pango-dev giflib-dev
+
+
+cd /ql/scripts && npm install -g pnpm
+cd /ql/scripts && npm install -g @otplib/preset-default
+cd /ql/scripts && npm install -g js-base64
+cd /ql/scripts && npm install -g fund
+cd /ql/scripts && npm install -g jsdom
+cd /ql/scripts && npm install -g form-data
+cd /ql/scripts && npm install -g tough-cookie
+cd /ql/scripts && npm install -g axios date-fns
+cd /ql/scripts && npm install -g crypto-js
+cd /ql/scripts && npm install -g crypto -g
+cd /ql/scripts && npm install -g download
+cd /ql/scripts && npm install -g typescript
+cd /ql/scripts && npm install -g png-js
+
+cd /ql/scripts && npm install -g got@11
+cd /ql/scripts && npm install -g ts-node
+cd /ql/scripts && npm install -g @types/node
+cd /ql/scripts && npm install -g canvas
+cd /ql/scripts && npm install -g ts-md5
+cd /ql/scripts && npm install -g ws
+
+
